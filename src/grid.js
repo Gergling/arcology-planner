aef6ee78544b08a9cd5b80333b5552390e3d9e01
Greg Davies <greg.davies@datix.co.uk>
2020-01-06 17:39:29 +0000

    Grid functions and spec.

---
+++ src/grid.js
@@ -0,0 +1,66 @@
+class Grid {
+  constructor() {
+    this._elements = [];
+    this._elementSize = 1;
+  }
+  getElements() {
+    return this._elements;
+  }
+  getElement(x, y, callback) {
+    const element = this._elements.find(gridElement => x === gridElement.x && y === gridElement.y);
+    if (callback === undefined) {
+      return element;
+    } else {
+      callback(element);
+      return this;
+    }
+  }
+  setElement(x, y) {
+    const element = this.getElement(x, y);
+    if (element === undefined) {
+      this._elements.push(new GridElement(x, y, this));
+    }
+    return this;
+  }
+
+  setElementSize(size) {
+    this._elementSize = size;
+    return this;
+  }
+  getElementSize() {
+    return this._elementSize;
+  }
+}
+
+class GridElement {
+  constructor(x, y, grid) {
+    this._x = x;
+    this._y = y;
+    this._grid = grid;
+  }
+
+  get x() {
+    return this._x;
+  }
+  get y() {
+    return this._y;
+  }
+  get grid() {
+    return this._grid;
+  }
+  set x(value) {
+    throw new Error('Cannot set x.');
+  }
+  set y(value) {
+    throw new Error('Cannot set y.');
+  }
+  set grid(value) {
+    throw new Error('Cannot set grid.');
+  }
+}
+
+function instantiate() {
+  return new Grid();
+}
+
+module.exports = instantiate;
---
+++ src/grid.spec.js
@@ -0,0 +1,42 @@
+const service = require('./grid');
+
+describe('Grid', () => {
+  describe('Basic Getting and Setting', () => {
+    test('#getElements returns an array', () => {
+      const grid = service();
+      expect(grid.getElements().constructor).toEqual([].constructor);
+    });
+  
+    test('#setElement and #getElement set an element and subsequently return it', () => {
+      const grid = service();
+      grid.setElement(1, 2).getElement(1,2, gridElement => {
+        expect(gridElement.x).toEqual(1);
+        expect(gridElement.y).toEqual(2);
+      });
+    });
+  
+    test('#getElements returns an array of all items which have been set', () => {
+      const gridElements = service()
+        .setElement(1, 2)
+        .setElement(2, 3)
+        .getElements();
+
+      expect(gridElements[0].x).toEqual(1);
+      expect(gridElements[0].y).toEqual(2);
+      expect(gridElements[1].x).toEqual(2);
+      expect(gridElements[1].y).toEqual(3);
+    });
+  });
+
+  describe('Grid Attributes', () => {
+    test('#setElementSize sets a size and #getElementSize returns it', () => {
+      expect(service()
+          .setElementSize(10)
+          .getElementSize())
+        .toEqual(10);
+    });
+  });
+
+  // Consider a section for chaining.
+  // Also need a section for editing grid elements.
+});
