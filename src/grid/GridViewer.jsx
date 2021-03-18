348a21d8e823cf417269039a35df1f07d882dad1
Greg Davies <greg.davies@datix.co.uk>
2021-03-18 17:37:27 +0000

    Dump

--- src/grid/GridViewer.jsx
+++ src/grid/GridViewer.jsx
@@ -134,29 +134,6 @@ function setupFactory(gridView) {
   }
   return setup;
 }
-// const setup = (ctx, forceUpdate) => {
-//   const line = (x1, y1, x2, y2) => {
-//     ctx.beginPath();
-//     ctx.moveTo(x1, y1);
-//     ctx.lineTo(x2, y2);
-//     ctx.lineWidth = 1;
-//     // ctx.strokeStyle = '#ff0000';
-//     ctx.stroke();
-//   }
-
-//   const verticalLine = x => {
-//     line(x, 0, x, ctx.canvas.height);
-//   }
-//   const horizontalLine = y => {
-//     line(0, y, ctx.canvas.width, y);
-//   }
-
-//   const vLines = ctx.canvas.width / gridSquareSize;
-//   const hLines = ctx.canvas.height / gridSquareSize;
-//   const map = (junk, idx) => idx * gridSquareSize;
-//   Array.from({length: vLines}).map(map).forEach(idx => verticalLine(idx));
-//   Array.from({length: hLines}).map(map).forEach(idx => horizontalLine(idx));
-// };
 
 export default class GridViewer extends Component {
   constructor(props) {
